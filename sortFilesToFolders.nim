2960194c20d19423f6fef3b63f00449deedfc2eb
ASNordroid <n3tstrang3r@yandex.ru>
2018-10-25 03:40:46 +0300

    Add sortFilesToFolders.nim

---
+++ sortFilesToFolders.nim
@@ -0,0 +1,39 @@
+import os, ospaths, strutils
+
+proc sortByExt(rootDir: string) =
+    for oldFilePath in walkDirRec(rootDir):
+        let newDirName = joinPath(rootDir, splitFile(oldFilePath).ext[1..^1])
+        # echo newDir
+        discard existsOrCreateDir(newDirName)
+        # echo joinPath(newDir, extractFilename(oldFilePath))
+        moveFile(oldFilePath, joinPath(newDirName, extractFilename(oldFilePath)))
+
+proc sortBy1000(rootDir:string) =
+    var curDir = 1
+    var count = 0
+
+    createDir(joinPath(rootDir, "__" & curDir.intToStr()))
+    for oldFilePath in walkDirRec(rootDir):
+        if count < 1000:
+            count += 1
+            #moveFile(oldFilePath, joinPath(rootDir, "__" & curDir.intToStr(), extractFilename(oldFilePath)))
+            echo count, " ", joinPath(rootDir, "__" & curDir.intToStr(), extractFilename(oldFilePath))
+        else:
+            count = 0
+            curDir += 1
+            #createDir(joinPath(rootDir, "__" & curDir.intToStr()))
+            echo curDir
+
+proc findSameNames(rootDir: string) =
+    var names = newSeq[string]()
+    var count = 0
+    for oldFilePath in walkDirRec(rootDir):
+        var name = extractFilename(oldFilePath)
+        if name notin names:
+            count += 1
+            names.add(name)
+        else:
+            echo name
+    echo count
+# sortBy1000()
+findSameNames("")

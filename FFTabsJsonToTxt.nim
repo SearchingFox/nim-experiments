cfc114eebcd95c7f6ff84c50da2a96f59467bc3a
ASNordroid <n3tstrang3r@yandex.ru>
2019-01-03 07:30:00 +0300

    Add new functions to FFTabsJsonToTxt

--- FFTabsJsonToTxt.nim
+++ FFTabsJsonToTxt.nim
@@ -4,11 +4,10 @@ type WebSite = object
     url:   string
     title: string
 
-type WebSites = OrderedTable[string, seq[WebSite]]
+type WebSites = Table[string, seq[WebSite]]
 
-proc getLinks(path: string): Table[string, seq[WebSite]] =
-    var result1 = {"0": @[WebSite(url : "", title : "")]}.toTable
-    result1.del("0")
+proc getLinks(path: string): WebSites = # Table[string, seq[WebSite]] = # auto
+    result = initTable[string, seq[WebSite]]()
     for i in parseFile(path):
         try:
             var t = newSeq[WebSite]()
@@ -18,44 +17,58 @@ proc getLinks(path: string): Table[string, seq[WebSite]] =
                         t.add(WebSite(url : z["url"].getStr, title : z["title"].getStr))
                     except KeyError:
                         t.add(WebSite(url : z["url"].getStr, title : ""))
-                    # if paramCount() > 1 and paramStr(1) == "--ext":
-            result1.add(format((i["date"].getInt.div(1000)).fromUnix, "yyMMdd"), t)
+            
+            # ! Unsafe because of multiple saves for one day
+            result.add((i["date"].getInt.div 1000).fromUnix.format("yyMMdd"), t)
         except:
             echo "Got exception ", repr(getCurrentException()), " with message ", getCurrentExceptionMsg()
 
-    return result1
+    return result
 
-let links = getLinks(r"C:\Users\Asus\Desktop\Сессии - 2018-12-22 08-35-04.json")
-stdout.write("> ")
+# proc findSimilarBuckets(s: seq[seq[WebSite]]) =
+#     for i in s:
+#         for j in s:
+#             if i != j:
+                
+
+let links = getLinks(r"C:\Users\Asus\Desktop\Сессии - 2018-12-29 04-54-30.json")
 var command = readLine(stdin)
+while command != "q":
+    stdout.write("> ")
+    command = readLine(stdin)
 
-if command == "help":
-    echo """
-    date - get all links from date
-    find - find word in links"""
-elif command == "date":
-    for k in toSeq(links.keys()).sorted((x, y) => cmp(x, y)):
-        echo k
-    let date = readLine(stdin)
-    for i in links[date]:
-        echo i.url, " - ", i.title
-elif command == "find":
-    let word = readLine(stdin).toLower
-    for v in links.values:
-        for i in v:
-            if word in i.url.toLower or word in i.title.toLower:
-                echo i.url, " - ", i.title
-elif command == "sets":
-    var links1 = toSeq(getLinks(r"").values()).concat.map(x => x.url).toSet
-    var links2 = toSeq(getLinks(r"").values()).concat.map(x => x.url).toSet
-    for i in links1 - links2:
-        echo i
-    echo "\n"
-    for i in links2 - links1:
-        echo i
-elif command == "tabs":
-    for i in toSeq(links.values()).sorted((x, y) => cmp(len(x), len(y))):
-        echo len(i)#, " - ", i
+    if command == "date":
+        for k in toSeq(links.keys()).sorted(cmp[string]):
+            echo k
+        let date = readLine(stdin)
+        for i in links[date]:
+            echo i.url, " - ", i.title
+    elif command == "find":
+        let word = readLine(stdin).toLower
+        for v in links.values:
+            for i in v:
+                if word in i.url.toLower or word in i.title.toLower:
+                    echo i.url, " - ", i.title
+    elif command == "sets":
+        var links1 = toSeq(getLinks(r"").values()).concat.map(x => x.url).toSet
+        var links2 = toSeq(getLinks(r"").values()).concat.map(x => x.url).toSet
+        for i in links1 - links2:
+            echo i
+        echo "\n"
+        for i in links2 - links1:
+            echo i
+    elif command == "tabs":
+        for i in toSeq(links.values()).sorted((x, y) => cmp(len(x), len(y))):
+            echo len(i)#, " - ", i
+    elif command == "links":
+        for i in links.values():
+            for j in i:
+                echo j.url
+    else:
+        echo """
+        q - quit
+        date - get all links from date
+        find - find word in links"""
 
 # echo "Enter full path:"
 # let path = readLine(stdin)

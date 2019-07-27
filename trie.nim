0be7238d88ee0396a739862bfa4fc0813da5913d
ASNordroid <n3tstrang3r@yandex.ru>
2019-07-27 22:00:34 +0300

    Update trie.nim

--- trie.nim
+++ trie.nim
@@ -1,21 +1,38 @@
-type Node = object
-    letter: char
-    children: seq[Node]
+import sequtils,
+       strutils
+
+type Node = ref object
+    letter:   char
+    children: seq[Node] #? ref Node, addr Node
 
 type Trie = ref object
     root: Node
 
+proc newTrie(): Trie =
+    let n = Node(letter: '\0', children: @[])
+    return Trie(root: n)
+
 # proc addNode(t: Trie, c: char) =
-proc addToTrie(t: Trie, s: string)
+proc addToTrie(t: Node, s: string) =
+    let l = s[0]
+    if l notin t.children.mapIt(it.letter):
+        let n = Node(letter: l, children: @[])
+        t.children.add(n)
+    for i in t.children:
+        if i.letter == l:
+            addToTrie(t.children[i], s[1..^1])
 
 
 proc printTrie(t: Trie) =
-    echo t.root.letter
-    for i in t.root.children:
-        echo "\t", i.letter
+    proc printHelper(n: Node, level: int = 0) =
+        echo repeat('\t', level), n.letter
+        for i in n.children:
+            printHelper(i, level + 1)
+    
+    printHelper(t.root)
 
 var u = Node(letter: 'u', children: newSeq[Node]())
-var n = Node(letter: 'a', children: [u])
+var n = Node(letter: 'a', children: @[u])
 var t = Trie(root: n)
 printTrie(t)
 # proc add(t: Trie, s: string): Trie =
